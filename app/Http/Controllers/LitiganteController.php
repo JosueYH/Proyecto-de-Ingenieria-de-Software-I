f29491e84a21643f4752b5d48adc0b7f2af8dbdb
JosueYH <Josueyalli098@protonmail.com>
2023-06-27 09:34:00 -0500

    Proyecto con todas las dependencias

---
+++ app/Http/Controllers/LitiganteController.php
@@ -0,0 +1,12 @@
+
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+//use App\Models\TLitigante;   //Importamos el modelo Litigante creado
+
+class CityController extends Controller
+{
+
+
+}
