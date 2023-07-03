7136a0c97e2fc71bea1fab7212064e22bba6a812
root <root@debian-BULLSEYE-live-builder-AMD64>
2023-07-03 00:28:23 -0500

    Proceso 5.5.5.1 login sin datos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>Login 10</title>
+  	<title>Login-Conciliador</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -12,57 +12,60 @@
 	<link rel="stylesheet" href="{{asset('plugins/iniciosesion/css/style.css')}}">
 
 	</head>
-	<body class="img js-fullheight" style="background-image: url({{asset('plugins/iniciosesion/images/bg.jpg')}});">
+	<body class="img js-fullheight" style="background-image: url({{asset('plugins/iniciosesion/images/bg.jpg')}})">
 	<section class="ftco-section">
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">Login #10</h2>
+                    <!--h2 class="heading-section">Login #10</h2-->
+                    <h2 class="heading-section">Sistema de Conciliación Prejudicial</h2>
 				</div>
 			</div>
 			<div class="row justify-content-center">
 				<div class="col-md-6 col-lg-4">
 					<div class="login-wrap p-0">
-		      	<h3 class="mb-4 text-center">Have an account?</h3>
-		      	<form action="#" class="signin-form">
+		      	<h3 class="mb-4 text-center">Ingrese con sus Credenciales</h3>
+
+                <form action="#" class="signin-form">
 		      		<div class="form-group">
-		      			<input type="text" class="form-control" placeholder="Username" required>
+		      			<input type="text" class="form-control" placeholder="Nombre de Usuario" required>
 		      		</div>
 	            <div class="form-group">
-	              <input id="password-field" type="password" class="form-control" placeholder="Password" required>
+	              <input id="password-field" type="password" class="form-control" placeholder="Contraseña" required>
 	              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
-	            </div>
-	            <div class="form-group">
-	            	<button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
-	            </div>
+                </div>
+
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
-		            	<label class="checkbox-wrap checkbox-primary">Remember Me
+		            	<label class="checkbox-wrap checkbox-primary">Recordarme
 									  <input type="checkbox" checked>
 									  <span class="checkmark"></span>
-									</label>
-								</div>
-								<div class="w-50 text-md-right">
-									<a href="#" style="color: #fff">Forgot Password</a>
-								</div>
-	            </div>
-	          </form>
-	          <p class="w-100 text-center">&mdash; Or Sign In With &mdash;</p>
-	          <div class="social d-flex text-center">
-	          	<a href="#" class="px-2 py-2 mr-md-1 rounded"><span class="ion-logo-facebook mr-2"></span> Facebook</a>
-	          	<a href="#" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span> Twitter</a>
-	          </div>
-		      </div>
-				</div>
+						</label>
+					</div>
+                </div>
+
+                <div class="form-group">
+	            	<button type="submit" class="form-control btn btn-primary submit px-3">INICIAR SESIÓN</button>
+                </div>
+
+            	<div class="mt-4">
+					<div class="d-flex justify-content-center links">
+						No tienes una Cuenta? <a href="#" class="ml-2">Registrarse</a>
+					</div>
+                </div>
+
+              </form>
+
+			</div>
 			</div>
 		</div>
 	</section>
-  <!--
+
   <script src="{{asset('plugins/iniciosesion/js/jquery.min.js')}}"></script>
   <script src="{{asset('plugins/iniciosesion/js/popper.js')}}"></script>
   <script src="{{asset('plugins/iniciosesion/js/bootstrap.min.js')}}"></script>
   <script src="{{asset('plugins/iniciosesion/js/main.js')}}"></script>
-  -->
+
 	</body>
 </html>
 
